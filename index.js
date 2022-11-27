4920dfa9768cd309e4490c0f1e1f083eea2e71c2
Robiul Islam <robiul215565@gmail.com>
2022-11-27 14:40:13 +0600

    Product added route done

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // External import
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 
 // Create a app
 const app = express();
@@ -10,62 +10,99 @@ const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
 // database connection
 const uri = `mongodb://localhost:27017`;
 
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 async function run() {
-    try{
-        const database = client.db('flashBack');
-        const usersCollection = database.collection('users');
+  try {
+    const database = client.db("flashBack");
+    const usersCollection = database.collection("users");
+    const productsCollection = database.collection("products");
 
-        /**
-         * Here all is users routes
-         * get users
-         * app.post()
-         * delete users
-         */
+    /**
+     * Here all is users routes
+     * get users
+     * app.post()
+     * delete users
+     */
 
-        // get all users
-        app.get('/users', async(req, res) =>{
-            const users = await usersCollection.find({}).toArray();
-            res.send(users)
-        })
+    // get all users
+    app.get("/users", async (req, res) => {
+      let query = {};
+      if (req.query.email) {
+        query = {
+          email: req.query.email,
+        };
+      }
+      const users = await usersCollection.find(query).toArray();
+      res.send(users);
+    });
 
-        // Create a users
-        app.post('/user', async(req, res)=>{
-            const user = req.body;
-            const addUser = await usersCollection.insertOne(user);
-            res.send(addUser)
-        })
+    // update verify
+    app.post("/user", async (req, res) => {
+      const filter = { email: req.query.email };
+      const updateDoc = {
+        $set: {
+          verified: true,
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
 
-        // delete a user
-        app.delete('/user/:id', async(req, res) =>{
-            const {id} = req.params;
-            const filter = {_id : ObjectId(id)}
-            const result = await usersCollection.deleteOne(filter);
-            res.send(result);
-        })
-    }
-    finally{
+    // Create a users
+    app.post("/user", async (req, res) => {
+      const user = req.body;
+      const addUser = await usersCollection.insertOne(user);
+      res.send(addUser);
+    });
 
-    }
+    // delete a user
+    app.delete("/user/:id", async (req, res) => {
+      const { id } = req.params;
+      const filter = { _id: ObjectId(id) };
+      const result = await usersCollection.deleteOne(filter);
+      res.send(result);
+    });
+
+    /**
+     * @products_route
+     *
+     */
+    // get all products
+    app.get("/products", async (req, res) => {
+      let query = {};
+
+      const users = await productsCollection.find(query).toArray();
+      res.send(users);
+    });
+    // create a products
+    app.post("/product", async (req, res) => {
+      const productDetails = req.body;
+      const result = await productsCollection.insertOne(productDetails);
+      res.send(result);
+    });
+  } finally {
+  }
 }
 
-
-run().catch(err => console.log(err.message))
+run().catch((err) => console.log(err.message));
 
 // Routes
 app.get("/health", (req, res) => {
   res.json({ message: "Server health is good" });
 });
-app.get('/', (req, res) =>{
-    res.send("<h1>Flash Back server is running</h1>")
-})
+app.get("/", (req, res) => {
+  res.send("<h1>Flash Back server is running</h1>");
+});
 
 // App listen on http://localhost:5000
 app.listen(port, () => {
