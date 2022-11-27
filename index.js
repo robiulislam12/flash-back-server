9200a56b194ff62180b51d64f6a8da79db98a43a
Robiul Islam <robiul215565@gmail.com>
2022-11-27 12:23:18 +0600

    Users routes done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // External import
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 // Create a app
@@ -10,6 +11,53 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json())
+
+// database connection
+const uri = `mongodb://localhost:27017`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try{
+        const database = client.db('flashBack');
+        const usersCollection = database.collection('users');
+
+        /**
+         * Here all is users routes
+         * get users
+         * app.post()
+         * delete users
+         */
+
+        // get all users
+        app.get('/users', async(req, res) =>{
+            const users = await usersCollection.find({}).toArray();
+            res.send(users)
+        })
+
+        // Create a users
+        app.post('/user', async(req, res)=>{
+            const user = req.body;
+            const addUser = await usersCollection.insertOne(user);
+            res.send(addUser)
+        })
+
+        // delete a user
+        app.delete('/user/:id', async(req, res) =>{
+            const {id} = req.params;
+            const filter = {_id : ObjectId(id)}
+            const result = await usersCollection.deleteOne(filter);
+            res.send(result);
+        })
+    }
+    finally{
+
+    }
+}
+
+
+run().catch(err => console.log(err.message))
 
 // Routes
 app.get("/health", (req, res) => {
